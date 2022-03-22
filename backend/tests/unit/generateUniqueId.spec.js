428c6a8d1d1443deb45d63aa077d7d2906fbea04
yourShadow <sipitalie@gmai.com>
2022-03-22 15:02:57 +0100

    update backEnd

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -3,7 +3,6 @@ const generateUniqueId = require("../../src/utils/generateUniqueid");
 describe('Generate Unique ID',() => {
     it('should generate an unique ID',() =>{
         const id = generateUniqueId();
-
         expect(id).toHaveLength(8);
 
     });
